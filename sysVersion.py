dda3ce68679cb4a285b4a0e7db342a7e40d82cd5
jnabucejo <janine_abucejo@ymail.com>
2024-10-12 17:43:11 +0800

    Exercise 2
    Use the sys module to output Python's version.

---
+++ sysVersion.py
@@ -0,0 +1,12 @@
+#Python Exercise
+'''
+2. Write a Python program to find out what version of Python you are using.
+'''
+# Import the sys module to access system-specific parameters and functions
+import sys  
+
+# Use the sys.version attribute to get the Python version and print it
+print("Python version:",sys.version)
+
+# Use the sys.version_info attribute to get detailed version information and print it
+print("Python version:",sys.version_info)
