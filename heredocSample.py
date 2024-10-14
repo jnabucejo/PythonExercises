0babbbde7ff687e02d072bd53608fbaf4482d3f3
Janine Abucejo <janine_abucejo@ymail.com>
2024-10-14 19:45:57 +0800

    Exercise 13
    Outputting sample "here document".

---
+++ heredocSample.py
@@ -0,0 +1,21 @@
+#Python Exercise
+'''
+13. Write a  Python program to print the following 'here document'.
+
+Sample string:
+a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example
+
+'''
+
+#Solution
+
+# Use triple double-quotes to create a multi-line string
+print("""
+a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example      
+""")
