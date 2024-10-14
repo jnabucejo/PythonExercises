a594c726aca41df71ecec586a68e7c4c121fea48
Janine Abucejo <janine_abucejo@ymail.com>
2024-10-14 09:29:51 +0800

    Exercise 11
    Output the docstring of a given built-in function name. Uses eval function.

---
+++ getDocString.py
@@ -0,0 +1,14 @@
+#Python Exercise
+'''
+11. Write a Python program to print the documents (syntax, description etc.) of Python built-in function(s).
+Sample function : abs()
+Expected Result :
+abs(number) -> number
+Return the absolute value of the argument.
+'''
+
+#Solution
+inf=input("Enter a built-in function name:")
+
+funcName=eval(inf)
+print(funcName.__doc__)
