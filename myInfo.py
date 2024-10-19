83c14e527f7a4cd639c7fc73319a2ff50dbfd0fe
Janine Abucejo <janine_abucejo@ymail.com>
2024-10-19 21:02:41 +0800

    Exercise 37
    Output personal details in three lines.

---
+++ myInfo.py
@@ -0,0 +1,12 @@
+#Python Exercise
+'''
+37. Write a Python program that displays your name, age, and address on three different lines.
+'''
+
+#Solution
+
+print("""
+Name: Sam
+Age : 1
+Address: Cavite
+""")
