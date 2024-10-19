953fcbfd8b74828985fa22441fbeff79a23a57b4
Janine Abucejo <janine_abucejo@ymail.com>
2024-10-19 21:46:34 +0800

    Exercise 39
    Calculate the future value using the given values.

---
+++ calcFutureVal.py
@@ -0,0 +1,28 @@
+#Python Exercise
+'''
+39. Write a Python program to compute the future value of a specified principal amount, rate of interest, and number of years.
+Test Data : amt = 10000, int = 3.5, years = 7
+Expected Output : 12722.79
+
+FV=P(1+r/n)^nt
+
+where:
+
+P = principal
+r = interest rate
+n = number of times interest applied per time period
+t =	number of time periods elapsed
+
+'''
+
+def calcFV() :
+    p = 10000
+    r = 3.5
+    t = 7
+    n = 1
+
+    FV = p * ( (1 + (r/n) ) ** (n * t))
+
+    return FV
+
+print("FV:",round(calcFV(),2))
