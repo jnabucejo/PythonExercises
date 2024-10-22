0b6ab4cfab8c4094c68e6b3339d40c7f87ca021d
Janine Abucejo <janine_abucejo@ymail.com>
2024-10-22 19:56:14 +0800

    Exercise 47
    Returns the CPU count.

---
+++ cpuCount.py
@@ -0,0 +1,13 @@
+#Python Exercise
+'''
+47. Write a  Python program to find out the number of  CPUs used.
+'''
+
+# Import the 'multiprocessing' module to work with multi-processing features.
+import multiprocessing
+
+# Use 'multiprocessing.cpu_count()' to determine the number of available CPU cores.
+cpu_count = multiprocessing.cpu_count()
+
+# Print the number of CPU cores available on the system.
+print(cpu_count)
